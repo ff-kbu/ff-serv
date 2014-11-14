e45b299dad7e384fec3af9dcecfd2f8392fd3149
Jan Luehr <yanosz@gmx.net>
2014-11-14 22:18:14 +0100

    Changed URL of stat-server

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,16 +1,13 @@
 module NodesHelper
 
   def ping_image_url(node)
-    url = 'https://kbu.freifunk.net/stats/ping/'
-    if(node.fw_version == "< 1.0")
-      url += 'ping_to_node-' + node.link_local_address + "_hour.png"
-    else
-      #http://stat.kbu.freifunk.net/nodes/159387616943362/stats/ping.png?secs=3600&width=400&height=160&no_summary=1
-      url = "http://stat.kbu.freifunk.net/nodes/#{node.mac.to_i(16)}/stats/ping.png?secs=3600&width=400&height=160&no_summary=2"
+    #Old: http://stat.kbu.freifunk.net/nodes/159387616943362/stats/ping.png?secs=3600&width=400&height=160&no_summary=1
+    #New: //kbu.freifunk.net/cserv/nodes/18687086762806/stats/ping.png?secs=86400&width=600&height=200&target=ping-fdd3:5d16:b5dd:3::6.rrd
+      url = "https://kbu.freifunk.net/cserv/nodes/#{node.mac.to_i(16)}/stats/ping.png?secs=3600&width=400&height=160&no_summary=2"
     end
   end
 
   def thruput_image_url(node,interface)
-    url = "http://stat.kbu.freifunk.net/nodes/#{node.mac.to_i(16)}/stats/interface/octets-#{interface}.png?secs=3600&width=400&height=160&no_summary=2"
+    url = "https://kbu.freifunk.net/cserv/nodes/#{node.mac.to_i(16)}/stats/interface/octets-#{interface}.png?secs=3600&width=400&height=160&no_summary=2"
   end
 end
