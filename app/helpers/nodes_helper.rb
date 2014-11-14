1adea69e7f112d362556cc5e8e9736ff684cc7cb
Jan Luehr <yanosz@gmx.net>
2014-11-14 22:23:46 +0100

    Fix typo

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -3,8 +3,7 @@ module NodesHelper
   def ping_image_url(node)
     #Old: http://stat.kbu.freifunk.net/nodes/159387616943362/stats/ping.png?secs=3600&width=400&height=160&no_summary=1
     #New: //kbu.freifunk.net/cserv/nodes/18687086762806/stats/ping.png?secs=86400&width=600&height=200&target=ping-fdd3:5d16:b5dd:3::6.rrd
-      url = "https://kbu.freifunk.net/cserv/nodes/#{node.mac.to_i(16)}/stats/ping.png?secs=3600&width=400&height=160&no_summary=2"
-    end
+    url = "https://kbu.freifunk.net/cserv/nodes/#{node.mac.to_i(16)}/stats/ping.png?secs=3600&width=400&height=160&no_summary=2"
   end
 
   def thruput_image_url(node,interface)
